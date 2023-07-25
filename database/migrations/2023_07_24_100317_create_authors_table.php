19ab1aff365660d791ee9cb4810c204ef5f85520
Rafał Pruś <rafalprues@gmail.com>
2023-07-25 12:36:07 +0200

    Add Author model and factories

--- database/migrations/2023_07_24_100317_create_authors_table.php
+++ database/migrations/2023_07_24_100317_create_authors_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('authors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('excerpt');
-            $table->text('description');
-            $table->string('category');
-            $table->integer('release_year');
-            $table->foreignId('author_id')->constrained();
             $table->timestamps();
         });
     }
@@ -28,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('authors');
     }
 };
--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -1,6 +1,6 @@
 
 @props(['article'])
-<a href="#" class="group">
+<a href="/articles/{{ $article->id }}" class="group">
     <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="h-full w-full object-cover object-center group-hover:opacity-75">
     </div>
