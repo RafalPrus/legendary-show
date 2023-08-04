e1a0df6a186ae6b0960783c3f18b65117da5458b
Rafał Pruś <rafalprues@gmail.com>
2023-08-04 11:28:13 +0200

    Refactor user update validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Arr;
 
 class UserController extends Controller
 {
@@ -15,35 +17,44 @@ class UserController extends Controller
     public function update(Request $request)
     {
         $user = auth()->user();
-        if ($request->file('avatar') !== null) {
-            $request->validate([
-                'avatar' => 'image|mimes:jpeg|max:2048'
-            ]);
-        }
+
         if ($user->name === $request->name && $user->email === $request->email && (!$request->file('avatar'))) {
             return back()
                 ->withErrors([
                     'input' => 'Provided data is the same. Nothing to update'
                 ]);
-        } elseif ($user->name === $request->name && $user->email !== $request->email) {
-            $attributes = $request->validate([
-                'email' => 'required|email|unique:users,email'
-            ]);
-        } elseif ($user->name !== $request->name && $user->email === $request->email) {
-            $attributes = $request->validate([
-                'name' => 'required|unique:users,name'
-            ]);
-        } else {
-            $attributes = $request->validate([
-                'name' => 'required|unique:users,name',
-                'email' => 'required|email|unique:users,email'
-            ]);
         }
 
 
+        $attributes = $request->validate([
+            'avatar' => 'image|mimes:jpeg|max:2048',
+            'name' => ['required', Rule::unique('users')->ignore($user)],
+            'email' => ['required', Rule::unique('users')->ignore($user)]
 
-        $user->update($attributes);
-        $user->addMediaFromRequest('avatar')->toMediaCollection('avatars');
+        ]);
+
+
+//        elseif ($user->name === $request->name && $user->email !== $request->email) {
+//            $attributes = $request->validate([
+//                'email' => 'required|email|unique:users,email'
+//            ]);
+//        } elseif ($user->name !== $request->name && $user->email === $request->email) {
+//            $attributes = $request->validate([
+//                'name' => 'required|unique:users,name'
+//            ]);
+//        } else {
+//            $attributes = $request->validate([
+//                'name' => 'required|unique:users,name',
+//                'email' => 'required|email|unique:users,email'
+//            ]);
+//        }
+
+
+
+        $user->update(Arr::except($attributes, ['avatar']));
+        if($request->file('avatar') !== null) {
+            $user->addMediaFromRequest('avatar')->toMediaCollection('avatars');
+        }
 
 
 
