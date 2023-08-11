137da19918c3c31a76bde32dbc88cad860f37d4f
Rafał Pruś <rafalprues@gmail.com>
2023-08-11 11:44:19 +0200

    Fix bug with error when no articles in database

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,11 +2,17 @@
 <x-layout heading="HOME">
     <div class="bg-white">
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
-            <x-article-grid :articles="$articles"/>
-            <!-- More products... -->
-            <div class="mt-4">
-                {{ $articles->links() }}
-            </div>
+            @if(count($articles) > 0)
+                <x-article-grid :articles="$articles"/>
+                <!-- More products... -->
+                <div class="mt-4">
+                    {{ $articles->links() }}
+                </div>
+            @else
+                <div class="mt-4">
+                    No content. Sorry!
+                </div>
+            @endif
         </div>
     </div>
 </x-layout>
